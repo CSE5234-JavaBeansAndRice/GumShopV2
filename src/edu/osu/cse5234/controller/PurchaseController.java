26e600e5493e9437fe3b87db10b4604c9783fd44
Simon John Turner <turner.1418@osu.edu>
2019-09-29 23:05:34 -0400

    added viewOrderEntryForm controller logic

--- src/edu/osu/cse5234/controller/PurchaseController.java
+++ src/edu/osu/cse5234/controller/PurchaseController.java
@@ -1,35 +1,57 @@
 package edu.osu.cse5234.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.osu.cse5234.model.Item;
+import edu.osu.cse5234.model.Order;
+import edu.osu.cse5234.model.PaymentInfo;
 
 @Controller
 @RequestMapping("/purchase")
 public class PurchaseController {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String viewOrderEntryPage(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// ... instantiate and set order object with items to display
-		return "OrderEntryForm";
-	}
+    @RequestMapping(method = RequestMethod.GET)
+    public String viewOrderEntryForm(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        Order order = new Order();
+        String[] gums = { "Bubble", "Spearmint", "Peppermint", "Wintergreen",
+                "Cinnamon" };
+        List<Item> items = new ArrayList<Item>();
+        for (int i = 0; i < 5; i++) {
+            Item item = new Item();
+            item.setName(gums[i]);
+            item.setPrice(i + .5);
+            item.setQuantity((i / 3) + 1);
+            items.add(item);
+        }
+        order.setItems(items);
+        request.getSession().setAttribute("order", order);
+        // ... instantiate and set order object with items to display
+        return "OrderEntryForm";
+    }
 
-	@RequestMapping(path = "/submitItems", method = RequestMethod.POST)
-	public String submitItems(@ModelAttribute("order") Order order, HttpServletRequest request) {
-		request.getSession().setAttribute("order", order);
-		return "redirect:/purchase/paymentEntry";
-	}
+    @RequestMapping(path = "/submitItems", method = RequestMethod.POST)
+    public String submitItems(@ModelAttribute("order") Order order,
+            HttpServletRequest request) {
+        request.getSession().setAttribute("order", order);
+        return "redirect:/purchase/paymentEntry";
+    }
 
-
-	@RequestMapping(path = "/paymentEntry", method = RequestMethod.GET)
-	public String viewPaymentEntryPage(HttpServletRequest request, HttpServletResponse response) {
-		request.setAttribute("payment", new PaymentInfo());	
-		return "PaymentEntryForm";
-	}
-
+    @RequestMapping(path = "/paymentEntry", method = RequestMethod.GET)
+    public String viewPaymentEntryPage(HttpServletRequest request,
+            HttpServletResponse response) {
+        request.setAttribute("payment", new PaymentInfo());
+        return "PaymentEntryForm";
+    }
 
     @RequestMapping(path = "/submitPayment", method = RequestMethod.POST)
     public String submitPaymentInfo() throws Exception {
@@ -39,7 +61,7 @@ public class PurchaseController {
 
     @RequestMapping(path = "/shippingEntry", method = RequestMethod.GET)
     public String displayShippingEntry() throws Exception {
-        return "HelloJSP";
+        return "ShippingEntryForm";
 
     }
 
@@ -51,7 +73,7 @@ public class PurchaseController {
 
     @RequestMapping(path = "/viewOrder", method = RequestMethod.GET)
     public String displayCompleteOrder() throws Exception {
-        return "HelloJSP";
+        return "ViewOrder";
 
     }
 
@@ -63,7 +85,7 @@ public class PurchaseController {
 
     @RequestMapping(path = "/viewConfirmation", method = RequestMethod.GET)
     public String displayConfirmation() throws Exception {
-        return "HelloJSP";
+        return "Confirmation";
 
     }
 }
--- src/edu/osu/cse5234/model/Item.java
+++ src/edu/osu/cse5234/model/Item.java
@@ -1,33 +1,31 @@
 package edu.osu.cse5234.model;
 
-import java.util.List;
-
 public class Item {
-	private String name;
-	private String price;
-	private String quantity;
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public void setPrice(String price) {
-		this.price = price;
-	}
-	
-	public void setQuantity(String quantity) {
-		this.quantity = quantity;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public String getPrice() {
-		return price;
-	}
-	
-	public String getQuantity() {
-		return quantity;
-	}
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
 }

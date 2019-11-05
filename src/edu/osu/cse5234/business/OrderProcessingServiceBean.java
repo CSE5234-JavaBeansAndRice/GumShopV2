0e0bdc51eddf3907801033ec79e91209c283d1df
yutheman <yu.1879@osu.edu>
2019-11-05 15:58:03 -0500

    lab 9 obj 3+4 done

--- src/edu/osu/cse5234/business/OrderProcessingServiceBean.java
+++ src/edu/osu/cse5234/business/OrderProcessingServiceBean.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.xml.ws.WebServiceRef;
 
 import com.chase.payment.CreditCardPayment;
 import com.chase.payment.PaymentProcessor;
+import com.ups.shipping.client.ShippingInitiationClient;
 
 import edu.osu.cse5234.model.LineItem;
 import edu.osu.cse5234.model.Order;
@@ -28,10 +31,10 @@ public class OrderProcessingServiceBean {
 	@PersistenceContext
 	EntityManager entityManager;
 	
-    @WebServiceRef(wsdlLocation = "http://localhost:9080/ChaseBankApplication/PaymentProcessorService?wsdl")
-    private PaymentProcessor paymentService;
+//    @WebServiceRef(wsdlLocation = "http://localhost:9080/ChaseBankApplication/PaymentProcessorService?wsdl")
+//    private PaymentProcessor paymentService;
 
-	
+    private static String shippingResourceURI = "http://localhost:9080/UPS/jaxrs";
     /**
      * Default constructor.
      */
@@ -42,6 +45,7 @@ public class OrderProcessingServiceBean {
     public String processOrder(Order order) {
     	InventoryService service = ServiceLocator.getInventoryService();
     	CreditCardPayment creditCardPayment = new CreditCardPayment(); 
+    	ShippingInitiationClient shipping = new ShippingInitiationClient(shippingResourceURI);
     	List<Item> itemList = new ArrayList<Item>();
     	for (LineItem line : order.getLineItems()) {
     		Item item = new Item();
@@ -50,16 +54,30 @@ public class OrderProcessingServiceBean {
     		item.setAvailableQuantity(line.getQuantity());
     		itemList.add(item);
     	}
-    	String status = paymentService.processPayment(creditCardPayment);
-    	if (Integer.valueOf(status) < 0) {
-    		return "ERROR";
-    	} else {
-	    	service.validateQuantity(itemList);
-			service.updateInventory(itemList);
-			entityManager.persist(order);
-			entityManager.flush();
-	        return status;
-    	}
+    	service.validateQuantity(itemList);
+		service.updateInventory(itemList);
+		entityManager.persist(order);
+		JsonObject requestJson = Json.createObjectBuilder()
+				.add("Organization", "GumShopV2")
+				.add("OrderRefId", order.getId())
+				.add("Zip", order.getShippingInfo().getZip())
+				.add("ItemsNumber", order.getLineItems().size())
+				.build();
+		JsonObject responseJson = shipping.invokeInitiateShipping(requestJson);
+		System.out.println("UPS accepted request? " + responseJson.getBoolean("Accepted"));
+		System.out.println("Shipping Reference Number: " +  responseJson.getInt("ShippingReferenceNumber"));
+		entityManager.flush();
+		return "4309320934";
+//    	String status = paymentService.processPayment(creditCardPayment);
+//    	if (Integer.valueOf(status) < 0) {
+//    		return "ERROR";
+//    	} else {
+//	    	service.validateQuantity(itemList);
+//			service.updateInventory(itemList);
+//			entityManager.persist(order);
+//			entityManager.flush();
+//	        return status;
+//    	}
     }
 
     public boolean validateItemAvailability(Order order) {
